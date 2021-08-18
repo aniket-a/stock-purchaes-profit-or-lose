ed10ef4baf08e421c66c5a481e94ea71473f3635
Aniket gupta <52057853+aniket-a@users.noreply.github.com>
2021-08-18 09:12:03 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const initialPrice = document.querySelector("#initial-price");
+const stockQuantity = document.querySelector("#quantity");
+const currentPrice = document.querySelector("#current-price");
+const submitBtn = document.querySelector("#tell-me");
+const outputBox = document.querySelector("#output");
+
+
+function calculateProfitAndLoss(initial, quantity, current){
+  if(initial > current){
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss / initial) * 100;
+    outputBox.innerText = `Hey the loss is: ${loss} and the percentage is" ${lossPercentage}%`;
+  }else if(current > initial){
+        var profit = (current - initial) * quantity;
+    var profitPercentage = (profit / initial) * 100;
+    outputBox.innerText = `Hey the Profit is: ${profit} and the percentage is" ${profitPercentage}%`;
+  
+  }else{
+    outputBox. innerText="Hey no pain no gain and no gain no pain!!";
+  }
+
+  }
+
+
+function submitHandler(){
+  var ip = Number(initialPrice.value);
+  var qty = Number(quantity.value);
+  var curr = Number(currentPrice.value);
+
+  calculateProfitAndLoss(ip, qty, curr);
+}
+
+
+
+
+
+
+submitBtn.addEventListener('click',  submitHandler);
